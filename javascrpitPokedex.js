40e3d420ce1487b5833796d09a815cb84e43858a
kevin <119064389+KevinSoto9@users.noreply.github.com>
2023-04-08 19:01:21 +0200

    Add files via upload

--- javascrpitPokedex.js
+++ javascrpitPokedex.js
@@ -1,21 +1,5 @@
 const ContenedorPokemon = document.querySelector('.Contenedor-Pokemon')
 
-const colors = {
-  fire: '#FFA05D',
-grass: '#8FD594',
-electric: '#FFE43B',
-water: '#7E97C0',
-ground: '#CAAC4D',
-rock: '#90642D',
-poison: '#9D5B9B',
-bug: '#EAFD71',
-dragon: '#97b3e6',
-psychic: '#FF96B5',
-flying: '#CDCDCD',
-fighting: '#FF5D5D',
-normal: '#FFFFFF'
-}
-
 function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
       .then((res) => res.json())
